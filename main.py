9a58077f06853d4ecf0af417d902186882da2aa2
Dhanraj-Patil <dhanraj68349@gmail.com>
2025-01-20 17:42:34 +0530

    implemented history route

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ from models import ProcessRequest, ProcessResponse
 from services import process_text
 
 app = FastAPI()
+history = []
 
 @app.post("/process")
 async def process_text_route(data: ProcessRequest):
     try:
         result = process_text(data.text)
+        history.append({"input": data.text, "result": result})
         return ProcessResponse(result=result)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/history")
+async def get_history():
+    return history
--- services.py
+++ services.py
@@ -1,9 +1,15 @@
 import google.generativeai as genai
+import os
+from dotenv import load_dotenv
 
-genai.configure(api_key="AIzaSyBSgVTCi_3nUqHE9v5leIeqB3mBMeSwwQI")
+load_dotenv()
+
+# Create a .env file to store the api key
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 def process_text(text):
+    # Generates sentiment analysis for the text provided by the user.
     content = model.generate_content(f"Provide sentiment analysis for the following text:\n{text}")
     return content.candidates[0].content.parts[0].text
     # print(content)
